4d2edb966c1ff89fe4387470cdcd990c6ee2abb9
Naveen-2021ucp1387 <naveenchouhan2001@gmail.com>
2024-07-12 02:32:28 +0530

    Setting up frontEnd

--- api/server.js
+++ api/server.js
@@ -11,12 +11,40 @@ const contractAddres = "0xf180FC8d8A0f624244067E7eDaDCB4A119A2A8aA";
 const contract = new web3.eth.Contract(ABI, contractAddres);
 
 
-const viewAllTask = async () => {
-  const allTask = await contract.methods.allTask().call();
-  console.log(allTask);
-}
+app.get("/api/ethereum/view-task/:taskId", async (req, res) => {
+  try {
+    const { taskId } = req.params;
+    const task = await contract.methods.viewTask(taskId).call();
+    const { id, name, date } = task;
+    const numId = Number(id);
+    const taskObj = {
+      numId, name, date
+    }
+    res.status(200).json({ status: 200, taskObj, message: "Task exist" });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: "Task ID does notexist" });
+  }
+})
 
-viewAllTask();
+
+
+app.get("/api/ethereum/view-all-task", async (req, res) => {
+  try {
+    const tasks = await contract.methods.allTask().call();
+
+    if (tasks.length > 0) {
+      const taskList = tasks.map(({ id, name, date }) => {
+        const taskId = Number(id);
+        return { taskId, name, date };
+      })
+      res.status(200).json({ status: 200, taskList, message: "Task exist" });
+    }else if(tasks.length < 0){
+      res.status(400).json({ status: 200, message: "No Task exist" });
+    }
+  } catch (error) {
+    res.status(500).json({ status: 500, message: "Task ID does notexist" });
+  }
+})
 app.listen(PORT, () => {
   console.log(`Listening on the port ${PORT}`);
 })
