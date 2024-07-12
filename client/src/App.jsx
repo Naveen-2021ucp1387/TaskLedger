3ba8fdd46a6258a1b953eb1e24366101db6574da
Naveen-2021ucp1387 <naveenchouhan2001@gmail.com>
2024-07-12 22:12:23 +0530

    frontEnd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+import {useState} from "react";
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import CreateTask from './pages/CreateTask'
+import Wallet from './pages/Wallet'
+import ViewAllTasks from './pages/ViewAllTasks'
+import UpdateTask from './pages/UpdateTask'
+import ViewTask from './pages/ViewTask'
+import DeleteTask from './pages/DeleteTask';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state,setState]=useState({web3:null,contract:null,account:null})
+  
+  const saveState=({web3,contract,account})=>{
+    setState({web3:web3,contract:contract,account:account})
+  }
+  const router = createBrowserRouter([
+    {path:'/',element:<Wallet saveState={saveState}/>},
+    {path:'/view-all-tasks',element:<ViewAllTasks/>},
+    {path:'/create-task',element:<CreateTask state={state}/>},
+    {path:'/view-task',element:<ViewTask/>},
+    {path:'/update-task',element:<UpdateTask state={state}/>},
+    {path:'/delete-task',element:<DeleteTask state={state}/>}
+  ])
 
   return (
     <>
-   
+     <RouterProvider router={router}/>
     </>
   )
 }
